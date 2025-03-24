13ab1fdd81d06ee8cbe6b07324971949ae25ac67
fardadmo <160783680+fardadmo@users.noreply.github.com>
2025-03-24 01:44:02 +0000

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 This is my newer app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
